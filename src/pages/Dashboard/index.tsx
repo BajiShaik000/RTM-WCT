af6f67533d7c7a9f723439e24c788fc507423507
Baji Shaik <Baji.sk@waferwire.com>
2025-03-24 18:51:07 +0530

    feat: more changes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -11,13 +11,13 @@ import { FC, useEffect, useMemo, useState } from "react";
 import styles from "./styles.module.scss";
 import ImageOne from "./dashboard1.png";
 import ImageTwo from "./dashboard2.png";
+import Image from "./dashboard.png"
 
 export const Dashboards: FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.subContainer}>
-        <img src={ImageTwo} alt="dashboard2" />
-        <img src={ImageOne} alt="dashboard1" />
+        <img src={Image} alt="dashboard2" />
       </div>
     </div>
   );
